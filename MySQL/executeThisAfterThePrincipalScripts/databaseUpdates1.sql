1d6e1a0e0df00affc24504f7fbbe242117175d9c
MariFerLT <mariferlopezt07@gmail.com>
2024-05-13 10:04:51 -0600

    nuevos cambios a la BD

--- MySQL/executeThisAfterThePrincipalScripts/databaseUpdates1.sql
+++ MySQL/executeThisAfterThePrincipalScripts/databaseUpdates1.sql
@@ -1,5 +1,20 @@
 ALTER TABLE users ADD UNIQUE (Email);
 
 
+
+
 -- nueva columna para saber si el chat tiene sus datos encriptados o no
 ALTER TABLE userchats ADD isDataEncrypted TINYINT(1) NOT NULL DEFAULT 0;
+
+
+
+
+SET SQL_SAFE_UPDATES = 0; -- esto es para desactivar el "Safe Update Mode"
+                        --  que evita la ejecución accidental de sentencias
+                        -- UPDATE o DELETE que podrían afectar a una gran cantidad de filas.
+
+
+
+
+-- nueva columna para guardar los mensajes encriptados
+ALTER TABLE Messages ADD COLUMN DataEncrypted VARBINARY(250);
