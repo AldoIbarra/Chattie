03c64d07834bb9e1996c75cae3f0a532a77b1698
MariFerLT <mariferlopezt07@gmail.com>
2024-05-12 22:06:48 -0600

    nueva columna para userchats

--- MySQL/executeThisAfterThePrincipalScripts/databaseUpdates1.sql
+++ MySQL/executeThisAfterThePrincipalScripts/databaseUpdates1.sql
@@ -1,1 +1,5 @@
 ALTER TABLE users ADD UNIQUE (Email);
+
+
+-- nueva columna para saber si el chat tiene sus datos encriptados o no
+ALTER TABLE userchats ADD isDataEncrypted TINYINT(1) NOT NULL DEFAULT 0;
